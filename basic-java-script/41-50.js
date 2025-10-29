442c0613506acc1c557bc26b08ab344af29fd7ed
Sandr163 <aleksandr.vyrmaskin@mail.ru>
2025-10-29 15:01:55 +0400

    lesson 41-50

---
+++ basic-java-script/41-50.js
@@ -0,0 +1,57 @@
+// Legacy JavaScript Algorithms and Data Structures
+// Setup
+const lastName = "Lovelace";
+
+// Only change code below this line
+const secondToLastLetterOfLastName = lastName[lastName.length - 2]; // Change this line
+
+// Word Blanks
+const myNoun = "dog";
+const myAdjective = "big";
+const myVerb = "ran";
+const myAdverb = "quickly";
+
+// Only change code below this line
+const wordBlanks = myNoun + " is " + myAdjective + " and " + myVerb + " very " + myAdverb; // Change this line
+console.log(wordBlanks)
+// Only change code above this line
+
+// Store Multiple Values in one Variable using JavaScript Arrays
+// Only change code below this line
+const myArray = ["one",(2),"butter"];
+
+// Nest one Array within Another Array
+// Only change code below this line
+const myArray = [["Balls",(23)],["Shoots",(32)]];
+
+
+// Access Array Data with Indexes
+const myArray = [50, 60, 70];
+const myData = myArray[0]
+
+
+// Modify Array Data With Indexes
+// Setup
+const myArray = [18, 64, 99];
+myArray[0] = 45
+// Only change code below this line
+
+
+// Access Multi-Dimensional Arrays With Indexes
+const myArray = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [[10, 11, 12], 13, 14],
+];
+
+const myData = myArray[2][1];
+
+// Manipulate Arrays With push Method
+// Setup
+const myArray = [["John", 23], ["cat", 2]];
+myArray.push(["dog", 3]);
+
+// Only change code below this line
+
+
