a83bbcf830553f08ecb8882650ec4b7c78aedbe7
Sandr163 <aleksandr.vyrmaskin@mail.ru>
2025-10-29 12:49:06 +0400

    lesson 33-40

---
+++ basic-java-script/33-40.js
@@ -0,0 +1,46 @@
+// Appending Variables to Strings
+// Change code below this line
+const someAdjective = "Круто!";
+let myStr = "freeCodeCamp is ";
+myStr += someAdjective
+
+
+// Find the Length of a String
+// Setup
+let lastNameLength = 0;
+const lastName = "Lovelace";
+
+// Only change code below this line
+lastNameLength = lastName.length;
+
+
+// Use Bracket Notation to Find the First Character in a String
+// Setup
+let firstLetterOfLastName = "";
+const lastName = "Lovelace";
+
+// Only change code below this line
+firstLetterOfLastName = lastName[0]; // Change this line
+
+
+// Understand String Immutability
+// Setup
+let myStr = "Jello World";
+
+// Only change code below this line
+myStr = "Hello World"; // Change this line
+// Only change code above this line
+
+// Use Bracket Notation to Find the Nth Character in a String
+// Setup
+const lastName = "Lovelace";
+
+// Only change code below this line
+const thirdLetterOfLastName = lastName[2]; // Change this line
+
+// Use Bracket Notation to Find the Last Character in a String
+// Setup
+const lastName = "Lovelace";
+
+// Only change code below this line
+const lastLetterOfLastName = lastName[lastName.length - 1]; // Change this line
