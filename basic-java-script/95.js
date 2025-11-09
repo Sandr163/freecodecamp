f69c3a88bf53f16ef7f25b2e0fc1c93728257355
Sandr163 <aleksandr.vyrmaskin@mail.ru>
2025-11-09 17:12:36 +0400

    lesson 95

---
+++ basic-java-script/95.js
@@ -0,0 +1,11 @@
+// Delete Properties from a JavaScript Object
+// Setup
+const myDog = {
+    "name": "Happy Coder",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["freeCodeCamp Campers"],
+    "bark": "woof"
+};
+delete myDog["tails"];
+
