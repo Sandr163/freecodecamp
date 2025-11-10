12d9b7c4077913cba4b0638557d84e9bb7e30e06
Sandr163 <aleksandr.vyrmaskin@mail.ru>
2025-11-10 16:29:29 +0400

    lesson 98

---
+++ basic-java-script/98.js
@@ -0,0 +1,24 @@
+// Manipulating Complex Objects
+// const myMusic = [
+//   {
+//     "artist": "Billy Joel",
+//     "title": "Piano Man",
+//     "release_year": 1973,
+//     "formats": [
+//       "CD",
+//       "8T",
+//       "LP"
+//     ],
+//     "gold": true
+//   },
+//   {
+//     "artist": "Queen",
+//     "title": "A Night at the Opera",
+//     "release_year": 1975,
+//     "formats": [
+//       "CD",
+//       "Cassette",
+//       "LP"
+//     ]
+//   }
+// ];
