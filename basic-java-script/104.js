56dbb6ad5442dabad6a229497b24e0dad661ff1d
Sandr163 <aleksandr.vyrmaskin@mail.ru>
2025-11-19 16:56:11 +0400

    lesson 104

--- basic-java-script/104.js
+++ basic-java-script/104.js
@@ -1,8 +1,8 @@
-// Iterate with JavaScript For Loops
+// Iterate Odd Numbers With a For Loop
 // Setup
 const myArray = [];
 
 // Only change code below this line
-for (let i = 1; i <= 5; i++){
+for (let i = 1; i <= 9; i += 2) {
     myArray.push(i);
 }
